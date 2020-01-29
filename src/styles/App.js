6cde2c5e63f81eae0b5f4512b32213fb1773f14f
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-01-29 19:48:15 -0300

    Iniciando a aplicação

---
+++ src/styles/App.js
@@ -0,0 +1,23 @@
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    content: {
+        flex: 1,
+        backgroundColor: '#606060',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    titulo: {
+        color: '#fff',
+        fontSize: 20
+    },
+    texto: {
+        color: 'orange',
+    },
+    botao:{
+        backgroundColor: 'white'
+    }
+  });
+
+  export default styles;
